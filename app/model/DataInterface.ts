c742d189868ea19c2679102e650a898c8bd553ff
Engin Bolat <engi.bolat@gmail.com>
2024-03-21 06:09:46 +0300

    dummy data and dummy model created

---
+++ app/model/DataInterface.ts
@@ -0,0 +1,7 @@
+export interface DataInterface {
+    id: number;
+    name: String;
+    details: String;
+    createdDate: string;
+    isDone: boolean;
+}
---
+++ app/utils/dummy.ts
@@ -0,0 +1,115 @@
+import { DataInterface } from "../model/DataInterface";
+
+export const data12: Array<DataInterface> = [
+    {
+        id: 1,
+        name: 'Name alanı1',
+        details: 'details alanı',
+        createdDate: '12 Şubat 15:45:32',
+        isDone: true,
+    },
+    {
+        id: 2,
+        name: 'Name alanı2',
+        details: 'details alanı',
+        createdDate: '12 Şubat 12:45:32',
+        isDone: false,
+    },
+    {
+        id: 3,
+        name: 'Name alanı3',
+        details: 'details alanı',
+        createdDate: '12 Şubat 10:45:32',
+        isDone: false,
+    },
+    {
+        id: 4,
+        name: 'Name alanı4',
+        details: 'details alanı',
+        createdDate: '12 Şubat 10:42:32',
+        isDone: true,
+    },
+    {
+        id: 5,
+        name: 'Name alanı5',
+        details: 'details alanı',
+        createdDate: '12 Şubat 08:45:32',
+        isDone: false,
+    },
+];
+
+export const data11: Array<DataInterface> = [
+    {
+        id: 1,
+        name: 'Name alanı1',
+        details: 'details alanı',
+        createdDate: '11 Şubat 15:45:32',
+        isDone: true,
+    },
+    {
+        id: 2,
+        name: 'Name alanı2',
+        details: 'details alanı',
+        createdDate: '11 Şubat 12:45:32',
+        isDone: false,
+    },
+    {
+        id: 3,
+        name: 'Name alanı3',
+        details: 'details alanı',
+        createdDate: '11 Şubat 10:45:32',
+        isDone: false,
+    },
+    {
+        id: 4,
+        name: 'Name alanı4',
+        details: 'details alanı',
+        createdDate: '11 Şubat 10:42:32',
+        isDone: true,
+    },
+    {
+        id: 5,
+        name: 'Name alanı5',
+        details: 'details alanı',
+        createdDate: '11 Şubat 08:45:32',
+        isDone: false,
+    },
+];
+
+export const data10: Array<DataInterface> = [
+    {
+        id: 1,
+        name: 'Name alanı1',
+        details: 'details alanı',
+        createdDate: '10 Şubat 15:45:32',
+        isDone: true,
+    },
+    {
+        id: 2,
+        name: 'Name alanı2',
+        details: 'details alanı',
+        createdDate: '10 Şubat 12:45:32',
+        isDone: false,
+    },
+    {
+        id: 3,
+        name: 'Name alanı3',
+        details: 'details alanı',
+        createdDate: '10 Şubat 10:45:32',
+        isDone: false,
+    },
+    {
+        id: 4,
+        name: 'Name alanı4',
+        details: 'details alanı',
+        createdDate: '10 Şubat 10:42:32',
+        isDone: true,
+    },
+    {
+        id: 5,
+        name: 'Name alanı5',
+        details: 'details alanı',
+        createdDate: '10 Şubat 08:45:32',
+        isDone: false,
+    },
+];
