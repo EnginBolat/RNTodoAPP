05faeadf26dcb0216cec55838f50673458b0cbc0
Engin Bolat <engi.bolat@gmail.com>
2024-03-21 20:11:19 +0300

    loading component

---
+++ app/component/loading/_layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ActivityIndicator } from "react-native"
+
+const CustomLoading = () => {
+    return <ActivityIndicator size="large" className="flex flex-1 items-center justify-center" />
+}
+
+export default CustomLoading;
