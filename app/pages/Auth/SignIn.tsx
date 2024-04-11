cafdbe0aff82cd2df9129e21f52855534deb4b15
Engin Bolat <engi.bolat@gmail.com>
2024-04-11 15:41:17 +0300

    created and done with forms

---
+++ app/pages/Auth/SignIn.tsx
@@ -0,0 +1,67 @@
+import React from "react"
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { PrimaryButton, PrimaryInput, PrimaryTitle } from "../../component";
+import { View, Text } from "react-native"
+
+interface SignInModel {
+    email: string;
+    password: string;
+}
+
+const SignIn = () => {
+    const SignInScheme = Yup.object().shape({
+        email: Yup.string().email('Invalid email').required('Required'),
+        password: Yup.string().required('Required'),
+    });
+
+    async function handleSubmit(value: SignInModel) {
+        console.log(`Email: ${value.email}\nPassword: ${value.password}`);
+    }
+
+    return <View className="flex flex-1 justify-center items-center p-3">
+        <PrimaryTitle title="Welcome!" style="m-3 mb-5 font-bold text-2xl w-full" />
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+            }}
+            validationSchema={SignInScheme}
+            onSubmit={values => handleSubmit(values)}
+        >
+            {({ handleChange, errors, touched, handleSubmit, values }) => (
+                <View className="w-full">
+                    <PrimaryInput
+                        value={values.email}
+                        placeHolder="Email"
+                        onChangeText={handleChange('email')}
+                        keyboardType="email-address"
+                    />
+                    {errors.email && touched.email ? <ErrorText title={errors.email} /> : null}
+                    <View className={`${errors.email && touched.email ? "py-0" : "py-1"} `} />
+                    <PrimaryInput
+                        value={values.password}
+                        placeHolder="Password"
+                        onChangeText={handleChange('password')}
+                        keyboardType="default"
+                        secureText={true}
+                    />
+                    {errors.password && touched.password ? <ErrorText title={errors.password} /> : null}
+                    <View className={`${errors.password && touched.password ? "py-0" : "py-1"} `} />
+                    <PrimaryButton
+                        title="Sign In"
+                        onPress={handleSubmit}
+                    />
+                </View>
+            )}
+        </Formik>
+    </View>
+}
+
+export default SignIn;
+
+
+const ErrorText: React.FC<{ title: string }> = ({ title }) => {
+    return <Text className="my-2 text-red-600">{title}</Text>
+}
