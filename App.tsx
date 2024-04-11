eb6c5b66077c6e1f797080c43e301d909c4d89c4
Engin Bolat <engi.bolat@gmail.com>
2024-04-11 15:40:40 +0300

    Main route changed

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { Home } from './app/pages'
+import { Home, SignIn } from './app/pages'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import store from './app/redux/store';
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <GestureHandlerRootView className='flex flex-1'>
-        <Home />
+        <SignIn />
       </GestureHandlerRootView>
     </Provider>
   );
