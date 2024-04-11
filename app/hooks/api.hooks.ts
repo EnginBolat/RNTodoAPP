858850a8627e4c1162d670ac3a96a881f430b864
Engin Bolat <engi.bolat@gmail.com>
2024-04-11 12:15:20 +0300

    created interface class for api calls

--- app/hooks/api.hooks.ts
+++ app/hooks/api.hooks.ts
@@ -1,19 +1,14 @@
-import axios, { HttpStatusCode } from "axios";
-import { DataInterface } from "../model/DataInterface"
+export interface IApiHooks {
+    get: Function;
+    add: Function;
+    update: Function;
+    delete: Function;
+}
 
-export async function addTodoService(title: string, desc: string, createdDate: string) {
-    try {
-        const response = await axios.post(`http://localhost:3000/api/v1/todo/`, {
-            title: title,
-            description: desc,
-            createdDate: createdDate,
-            isDone: false
-        });
-        if (response.status === HttpStatusCode.Created) {
-            console.log('Data Added In Service')
-            console.log(response.data);
-        }
-    } catch (error) {
-        console.error("Data Can't Added:", error);
-    }
+export class ApiHooks implements IApiHooks {
+    get() { console.log('get'); };
+    add() { console.log('add'); };
+    update() { console.log('update'); };
+    delete() { console.log('delete'); };
+
 }
