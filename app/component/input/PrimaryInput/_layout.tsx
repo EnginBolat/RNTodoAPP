7787f2ef9485ef6ec4e8254df1f4ba7924ad5f8b
Engin Bolat <engi.bolat@gmail.com>
2024-04-11 15:40:56 +0300

    add onChangeText

--- app/component/input/PrimaryInput/_layout.tsx
+++ app/component/input/PrimaryInput/_layout.tsx
@@ -4,12 +4,15 @@ import { KeyboardTypeOptions, TextInput } from 'react-native';
 
 const PrimaryInput: React.FC<{
     value: string;
-    setValue: React.Dispatch<React.SetStateAction<string>>;
+    setValue?: React.Dispatch<React.SetStateAction<string>>;
+    onChangeText: any;
     placeHolder: string;
     style?: string;
     keyboardType?: KeyboardTypeOptions;
     maxLength?: number
-}> = ({ value, setValue, placeHolder, style, keyboardType = 'default', maxLength = 120 }) => {
+    cursorColor?: string,
+    secureText?: boolean;
+}> = ({ value, setValue, placeHolder, onChangeText, style, secureText = false, cursorColor = "black", keyboardType = 'default', maxLength = 120 }) => {
 
     const defaultButtonStyle = 'items-center justify-center p-4 bg-white rounded-xl w-full border-black border text-neutral-500';
     if (!style) {
@@ -17,13 +20,16 @@ const PrimaryInput: React.FC<{
     }
 
     return <TextInput
+        cursorColor={cursorColor}
+        selectionColor={cursorColor}
         className={style}
         placeholder={placeHolder}
         keyboardType={keyboardType}
         value={value}
         maxLength={maxLength}
+        secureTextEntry={secureText}
         onChangeText={(text: string) => {
-            setValue(text);
+            !setValue ? onChangeText(text) : setValue(text);
         }}
     />
 }
